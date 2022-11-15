c10cfc661f1d08fb95e774ffc48aa56f689c3015
MaaHi <MaaHi@DESKTOP-RV6HMDD>
2022-11-15 21:59:57 +0530

    comit2

--- MavenProject/src/test/java/class1.java
+++ MavenProject/src/test/java/class1.java
@@ -1,12 +1,7 @@
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
-
-	import org.testng.annotations.AfterClass;
-	import org.testng.annotations.AfterMethod;
-	import org.testng.annotations.BeforeClass;
-	import org.testng.annotations.BeforeMethod;
-	import org.testng.annotations.Test;
-
-	public class class1 {
+public class class1 {
 			@BeforeClass
 			public void beforeClass()
 			{
@@ -14,7 +9,11 @@
 			}
 			
 
-			
+			@Test
+			public void test()
+			{
+				System.out.println("Tc1");
+			}
 			
 
 	}
